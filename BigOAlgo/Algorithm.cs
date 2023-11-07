bb080d59ebd661ff39508e59b3afe65cb5b88c70
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-11-07 16:16:08 -0500

    added DetermineItems method

--- BigOAlgo/Algorithm.cs
+++ BigOAlgo/Algorithm.cs
@@ -5,23 +5,42 @@ namespace BigOAlgo
 	{
 		public Algorithm()
 		{
-			
-		}
 
+		}
+		//Time Complexity is Constant 
 		public static bool GetNumber(int number)
 		{
 
 			if (number % 2 == 0)
 			{
 				Console.WriteLine($"{number} is even");
-                return true;
-            }
-            else
+				return true;
+			}
+			else
 			{
 				Console.WriteLine($"{number} is odd");
 				return false;
 			}
 		}
+		//Task 2 Less Than 100
+		// Time Complexity is Linear 
+		public static bool DetermineItems(List<int> fullList)
+
+		{
+			foreach (int number in fullList)
+			{
+				if (number >= 100)
+				{
+					return false;
+				}
+				else
+				{
+
+				}
+				
+			} return true;
+        }
+
 	}
 }
 
--- BigOAlgo/Program.cs
+++ BigOAlgo/Program.cs
@@ -8,7 +8,19 @@
             
             Algorithm.GetNumber(10);
             Algorithm.GetNumber(15);
+            
+            var newList = new List<int>
+            {
+              1,
+              3,
+              58,
+              75,
+              100,
+              150,
+              200
+            };
+            Algorithm.DetermineItems(newList);
         }
-    }
+    } 
 }
 
